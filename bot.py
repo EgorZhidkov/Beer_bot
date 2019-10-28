b4ec89d2d50f964fb78b648d9e55cbe73ca3fc78
Egor Zhidkov <e.jidkov@gmail.com>
2019-10-28 18:30:28 +0300

    пытасюь наладить алгоритм

--- bot.py
+++ bot.py
@@ -2,19 +2,31 @@ import telebot
 import random
 from telebot.types import Message
 from classbeer import Beer
+
 TOKEN = '927282765:AAHUsLXygHVabpQvNyl1IS-XD219jNDTSKw'
 bot = telebot.TeleBot(TOKEN)
-first_beer = Beer(54.90,"Velkopopovicky Kozel", 3.7, "Темное", 0.45)
-second_beer = Beer(54.90, "Velkopopovicky Kozel", 4 , "Светлое", 0.45)
+first_beer = Beer(54.80, "Velkopopovicky Kozel", 3.7, "Темное", 0.45)
+second_beer = Beer(54.90, "Velkopopovicky Kozel", 4, "Светлое", 0.45)
 third_beer = Beer(39.90, "Хамовники", 4.5, "Светлое", 0.47)
 a = "cjbgif"
+myArray = [first_beer, second_beer, third_beer]
+index = 0
+def search(message):
+    for b in range(myArray.__len__()):
+        if message.text == myArray[b].get_value():
+            test = b
+            repr(myArray[test])
+
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Здравствуйте,я бот по подбору пива, дальше от вас требуется ввести параметры,по одному в каждое сообщение: цена, название, крепость, вид, размер ")
+    bot.reply_to(message, "Здравствуйте,я бот по подбору пива, дальше от вас требуется ввести параметры,по одному в "
+                          "каждое сообщение: цена, название, крепость, вид, размер ")
+
 
 @bot.message_handler(content_types=['text'])
-def check_id_sticker(message: Message):
-    bot.send_message(message.chat.id, a)
+def check_id_sticker(message):
+    bot.send_message(message.chat.id, search(message))
 
 
 bot.polling(timeout=60)
--- classbeer.py
+++ classbeer.py
@@ -15,4 +15,17 @@ class Beer(object):
         return "Цена = " + str(self.value) + "; Название - " + str(self.title) + "; Крепость алкоголя = " + str \
             (self.degree) + "; Вид: " + str(self.species) + "; Размер = " + str(self.size)
 
+    def get_value(self):
+        return self.value
 
+    def get_title(self):
+        return self.title
+
+    def get_degree(self):
+        return self.degree
+
+    def get_species(self):
+        return self.species
+
+    def get_size(self):
+        return self.size
